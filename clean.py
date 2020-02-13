5a8e49ebc131b45fe7d6d13dd028a61715981b39
Rebecca Brunner <brunner@bgsu.edu>
2020-02-13 16:43:09 -0500

    Turn script into class

--- clean.py
+++ clean.py
@@ -1,19 +1,53 @@
 import os
 import re
-import subprocess
-from settings import *
+import argparse
 
-# Get list of files
-file_list = []
-for subdir, dirs, files in os.walk(ROOT_DIR):
-    for file in files:
-        file_list.append(file)
+'''Cleaner
+Cleans a repo of unwanted files - useful for getting rid of large binary files
+Arguments: root -> string: The root you want to run cleaner in (Works in working directory or .git directory)
+           match -> string: regex expression of what you want to KEEP
+Returns  : Repository cleaned of unwanted blobs
+'''
+class Cleaner:
+    def __init__(self, root, match):
+        self.root = root
+        self.match = match
+        self.file_list = []
+        self.names = []
 
-# Get list of files not matching
-names = [file for file in file_list if not re.match(MATCH, file)]
+    # Get list of files
+    def get_files(self):
+        file_list = []
+        for subdir, dirs, files in os.walk(self.root):
+            dirs[:] = [d for d in dirs if d not in '.git']
+            for file in files:
+                self.file_list.append(file)
 
-# Call git command on files
-for name in names:
-    os.system(f'git filter-branch --tree-filter "git rm {name} -f" -f')
-os.system('git reflog expire --expire-unreachable=now --all')
-os.system('git gc --aggressive --prune=now')
+    # Get list of files not matching
+    def get_names(self):
+        self.names = [file for file in self.file_list if not re.match(self.match, file)]
+
+    # Call git command on files
+    def run_git(self):
+        os.chdir(self.root)
+        for name in self.names:
+            os.system(f'git filter-branch --tree-filter "git rm {name} -f" -f')
+        os.system('git reflog expire --expire-unreachable=now --all')
+        os.system('git gc --aggressive --prune=now')
+    
+    def clean(self):
+        self.get_files()
+        self.get_names()
+        self.run_git()
+
+# Test drive code
+# Uncomment code below to test
+# parser = argparse.ArgumentParser()
+# parser.add_argument('root')
+# parser.add_argument('match')
+# args = parser.parse_args()
+# root = args.root
+# match = args.match
+
+# cleaner = Cleaner(root, match)
+# cleaner.clean()
