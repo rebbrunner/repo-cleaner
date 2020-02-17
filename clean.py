02697ceae5ac465db55c2f541d367cec50481ed6
Rebecca Brunner <brunner@bgsu.edu>
2020-02-17 15:46:54 -0500

    Update script to use filter-repo

--- clean.py
+++ clean.py
@@ -14,40 +14,20 @@ class Cleaner:
         self.match = match
         self.file_list = []
         self.names = []
-
-    # Get list of files
-    def get_files(self):
-        file_list = []
-        for subdir, dirs, files in os.walk(self.root):
-            dirs[:] = [d for d in dirs if d not in '.git']
-            for file in files:
-                self.file_list.append(file)
-
-    # Get list of files not matching
-    def get_names(self):
-        self.names = [file for file in self.file_list if not re.match(self.match, file)]
-
-    # Call git command on files
-    def run_git(self):
-        os.chdir(self.root)
-        for name in self.names:
-            os.system(f'git filter-branch --tree-filter "git rm {name} -f" -f')
-        os.system('git reflog expire --expire-unreachable=now --all')
-        os.system('git gc --aggressive --prune=now')
     
     def clean(self):
-        self.get_files()
-        self.get_names()
-        self.run_git()
+        os.chdir(self.root)
+        os.system(f'git filter-repo --path-regex {match} --force')
+        
 
 # Test drive code
 # Uncomment code below to test
-# parser = argparse.ArgumentParser()
-# parser.add_argument('root')
-# parser.add_argument('match')
-# args = parser.parse_args()
-# root = args.root
-# match = args.match
+parser = argparse.ArgumentParser()
+parser.add_argument('root')
+parser.add_argument('match')
+args = parser.parse_args()
+root = args.root
+match = args.match
 
-# cleaner = Cleaner(root, match)
-# cleaner.clean()
+cleaner = Cleaner(root, match)
+cleaner.clean()
